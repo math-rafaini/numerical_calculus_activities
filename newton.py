4cbac84c0eaba940ef2ba7a441d61a5f9d2c2dbf
Matheus Rafaini <107150354+mrafaini@users.noreply.github.com>
2023-03-15 23:08:45 +0000

    Update newton method

--- newton.py
+++ newton.py
@@ -1,11 +1,9 @@
 from collections.abc import Callable
 
-# INPUT
 init_alpha = 0.15
 precision = 10**(-10)
 num_iterations = 1000
 
-#FUNCTION
 def newton(function:Callable[[float], float], derivative:Callable[[float], float], approximation: float, max_iterations: int, tolerance: float) -> float:
     i = 0
     stop_criteria = function(approximation + tolerance) * function(approximation - tolerance)
