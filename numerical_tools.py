d8bc845db0f77c37e5a6f5dc7cb8b565db697bad
GuiLeme <leme.guilherme.p@gmail.com>
2023-03-19 17:39:38 -0300

    Solved task 2

--- numerical_tools.py
+++ numerical_tools.py
@@ -4,7 +4,7 @@ import pandas as pd
 pd.set_option("display.precision", 45)
 
 
-def newton_method(function:Callable[[float], float], derivative:Callable[[float], float], approximation: float, max_iterations: int, tolerance: float) -> float:
+def newton_method(function:Callable[[float], float], derivative:Callable[[float], float], approximation: float, max_iterations: int, tolerance: float) -> list:
     i = 0
     stop_criteria = function(approximation + tolerance) * function(approximation - tolerance)
     approximations_list = []
@@ -21,8 +21,64 @@ def newton_method(function:Callable[[float], float], derivative:Callable[[float]
         i+=1
         approximations_list.append(approximation)
 
-def bisection_method(function:Callable[[float], float], max_iterations: int, tolerance: float):
-    pass
+def bisection_method(function:Callable[[float], float], max_iterations: int, tolerance: float, 
+                     lower_bound: float, upper_bound: float):
+    minimal_i = lower_bound
+    maximal_i = upper_bound
+    alpha_next = (minimal_i + maximal_i) / 2
+    approximations_list = [alpha_next]
+    i = 0
+    while function(alpha_next - tolerance) * function(alpha_next + tolerance) >= 0 and i < max_iterations:
+        if function(alpha_next)*function(minimal_i) < 0:
+            maximal_i = alpha_next
+        elif function(alpha_next)*function(minimal_i) == 0:
+            if function(alpha_next) == 0:
+                return approximations_list
+            else: 
+                approximations_list.append(minimal_i)
+                return approximations_list
+        elif function(alpha_next)*function(minimal_i) > 0:
+            minimal_i = alpha_next
+        alpha_next = (minimal_i + maximal_i) / 2
+        approximations_list.append(alpha_next)
+        i += 1
+    
+    return approximations_list
+
+def newton_method_performance_focused(function:Callable[[float], float], derivative:Callable[[float], float], approximation: float, max_iterations: int, tolerance: float) -> float:
+    i = 0
+    stop_criteria = function(approximation + tolerance) * function(approximation - tolerance)
+    while(i <= max_iterations):
+        if(function(approximation) == 0.0):
+            return approximation
+            
+        elif(stop_criteria < 0):
+            return approximation
+
+        approximation = approximation - (function(approximation)/derivative(approximation))
+        stop_criteria = function(approximation + tolerance) * function(approximation - tolerance)
+        i+=1
+
+def bisection_method_performance_focused(function:Callable[[float], float], max_iterations: int, tolerance: float, 
+                     lower_bound: float, upper_bound: float):
+    minimal_i = lower_bound
+    maximal_i = upper_bound
+    alpha_next = (minimal_i + maximal_i) / 2
+    i = 0
+    while function(alpha_next - tolerance) * function(alpha_next + tolerance) >= 0 and i < max_iterations:
+        if function(alpha_next)*function(minimal_i) < 0:
+            maximal_i = alpha_next
+        elif function(alpha_next)*function(minimal_i) == 0:
+            if function(alpha_next) == 0:
+                return alpha_next
+            else: 
+                return minimal_i
+        elif function(alpha_next)*function(minimal_i) > 0:
+            minimal_i = alpha_next
+        alpha_next = (minimal_i + maximal_i) / 2
+        i += 1
+    
+    return alpha_next
 
 def calculate_integral_trapezium_method(f, m, a, b):
     h = (b-a)/m
--- task_2.py
+++ task_2.py
@@ -1,4 +1,10 @@
-from numerical_tools import newton_method, calculate_integral_trapezium_method
+from time import time
+
+from numerical_tools import (newton_method, 
+                            calculate_integral_trapezium_method,
+                            bisection_method,
+                            newton_method_performance_focused,
+                            bisection_method_performance_focused)
 
 def f(x):
     return 105*x**2*(1-x)**4
@@ -10,8 +16,31 @@ def F(tau):
 def derivative_of_F(tau):
     return f(tau)
 
-list_of_approximations_newton_method = newton_method(F, derivative_of_F, 0.5, 1000, 10E-10)
-print(len(list_of_approximations_newton_method), list_of_approximations_newton_method[-1])
+# The folowing lines are commented to avoid poluting the task's output
+# print("---------------------")
+# print("Newton's method")
+# list_of_approximations_newton_method = newton_method(F, derivative_of_F, 0.5, 1000, 10E-10)
+# print(len(list_of_approximations_newton_method), list_of_approximations_newton_method[-1])
 
-list_of_approximations_bisection_method = newton_method(F, derivative_of_F, 0.5, 1000, 10E-10)
-print(len(list_of_approximations_bisection_method), list_of_approximations_bisection_method[-1])
+# print("---------------------")
+# print("Bisection method")
+# list_of_approximations_bisection_method = bisection_method(F, 1000, 10E-10, 0, 1)
+# print(len(list_of_approximations_bisection_method), list_of_approximations_bisection_method[-1])
+
+# -----------------------------
+# item number 1
+# -----------------------------
+
+print("---------------------")
+print("Newton's method")
+before_timestamp = time()
+newton = newton_method_performance_focused(F, derivative_of_F, 0.5, 1000, 10E-10)
+print("Time spent in newton's method: ", time() - before_timestamp)
+print("Result from newton's method: ", newton)
+
+print("---------------------")
+print("Bisection method")
+before_timestamp = time()
+bisection = bisection_method_performance_focused(F, 1000, 10E-10, 0, 1)
+print("Time spent in bisection method: ",  time() - before_timestamp)
+print("Result from bisection method: ", bisection)
